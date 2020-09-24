b7f680d087cb4ae69c949f86a7d6ce15d2cc6821
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-09-24 15:17:37 +0530

    Update first.sh

--- LAB1/first.sh
+++ LAB1/first.sh
@@ -1,1 +1,20 @@
+#! /bin/sh
+# Sample Shell Script
 
+echo "Today's Date: `date`"
+echo "My Shell: $SHELL"
+echo "This month's Calander: `cal`"
+echo "Today's Date: `date "+%d %m %y"`"
+
+
+Today's Date: Thu Sep 24 14:26:35 IST 2020
+My Shell: /bin/bash
+This month's Calander:    September 2020     
+Su Mo Tu We Th Fr Sa  
+       1  2  3  4  5  
+ 6  7  8  9 10 11 12  
+13 14 15 16 17 18 19  
+20 21 22 23 24 25 26  
+27 28 29 30           
+                      
+Today's Date: 24 09 20
