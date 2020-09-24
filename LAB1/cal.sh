71d9c177934661a311c8cdae4d8952dee5c58d76
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-09-24 15:25:29 +0530

    Update cal.sh

--- LAB1/cal.sh
+++ LAB1/cal.sh
@@ -16,3 +16,12 @@ echo "Multiplication: $product"
 
 quoteint=`echo $num1 / $num2|bc`
 echo "Division: $quoteint"
+
+Enter two numbers
+10
+5
+Addition: 15
+Subtraction: 5
+Multiplication: 50
+Division: 2
+
