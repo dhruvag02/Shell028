a7e0f50f4558eebaf27f013c8a54c6b22acb3c6e
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-10-01 15:16:05 +0530

    Update exprcomm.sh

--- Lab2/exprcomm.sh
+++ Lab2/exprcomm.sh
@@ -10,3 +10,12 @@ echo "Subtraction: `expr $NUM1 - $NUM2`"
 echo "Multiplication: `expr $NUM1 \* $NUM2`"
 echo "Division: `expr $NUM1 / $NUM2`"
 echo "Modulus: `expr $NUM1 % $NUM2`"
+
+Enter two numbers
+10
+5
+Addition: 15
+Subtraction: 5
+Multiplication: 50
+Division: 2
+Modulus: 0
