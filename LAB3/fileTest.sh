3c17769e9b7151ea4c3673e5d49ef2b2c445077a
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-10-08 15:12:38 +0530

    Update fileTest.sh

--- LAB3/fileTest.sh
+++ LAB3/fileTest.sh
@@ -24,4 +24,26 @@ then
 else
 	echo "specify two arguments aa two files"
 fi
+
+
+Programme: fileTest.sh
+Number of arguments specified: 2
+Arguments are: fil2 file
+Attributes of 1st file are displayed
+-rw-r--r-- 1 dhruv dhruv 16 Oct  1 14:36 fil2
+Attributes of 2nd file are displayed
+-rw-r--r-- 1 dhruv dhruv 115 Oct  8 15:05 file
+
+Programme: fileTest.sh
+Number of arguments specified: 2
+Arguments are: fil2 file2
+Attributes of 1st file are displayed
+-rw-r--r-- 1 dhruv dhruv 16 Oct  1 14:36 fil2
+file2 Not a file name
+
+Programme: fileTest.sh
+Number of arguments specified: 0
+Arguments are: 
+specify two arguments aa two files
+
 	
