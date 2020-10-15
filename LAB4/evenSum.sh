9bea47df02629b2dab7e9b99b5340f124d8a1cbc
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-10-15 15:02:10 +0530

    Update evenSum.sh

--- LAB4/evenSum.sh
+++ LAB4/evenSum.sh
@@ -11,4 +11,13 @@ do
 	I=`expr $I + 2`
 done
 echo "Sum of even numbers upto $NUM: $SUM"
+
+Enter a number
+7
+Sum of even numbers upto 7: 12
+
+Enter a number
+5
+Sum of even numbers upto 5: 6
+
  
