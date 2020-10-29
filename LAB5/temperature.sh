27fd65ea46093712287fd01eac7145729a242320
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-10-29 15:16:26 +0530

    Update temperature.sh

--- LAB5/temperature.sh
+++ LAB5/temperature.sh
@@ -9,3 +9,22 @@ echo "$TEM"
 C=`echo  $TEM \* 5 / 9|bc`
 echo "Temperature in Celcius: $C"
 
+
+
+Enter temperature in Fahrenheit
+-40
+-72
+Temperature in Celcius: -40
+
+Enter temperature in Fahrenheit
+30
+-2
+Temperature in Celcius: -1
+
+
+Enter temperature in Fahrenheit
+32
+0
+Temperature in Celcius: 0
+
+
