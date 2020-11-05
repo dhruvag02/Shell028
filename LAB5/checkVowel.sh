787b7e6bb98479a398f463bff583f4bf2a2f3c55
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-11-05 15:07:55 +0530

    Update checkVowel.sh

--- LAB5/checkVowel.sh
+++ LAB5/checkVowel.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 # Check for consonant or vowel
+# Using command line argument
 # Can be done using case statement also
 
 if [ $# -ne 1 ]
@@ -12,3 +13,13 @@ then
 else
 	echo "Entered Character is a consonant"
 fi 
+
+
+$ sh checkVowel.sh 
+Only one argument is required
+
+$ sh checkVowel.sh a
+Entered Character is a vowel
+
+$ sh checkVowel.sh d
+Entered Character is a consonant
