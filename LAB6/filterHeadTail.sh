5cc0d562ba5922242da0ca02f00e5dcdf1863940
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-11-26 15:34:08 +0530

    Update filterHeadTail.sh

--- LAB6/filterHeadTail.sh
+++ LAB6/filterHeadTail.sh
@@ -2,6 +2,30 @@
 # Use of filter commands(Head and tail)
 
 echo "Name of resultant file is File3. "
-
+echo "\n"
 `head -5 File1 >> File3`
+`tail -8 File2 >> File3`
+
+#Can be used
+#cat File1|head -5 >> File3
+#cat File2|head -15|tail -8 >> File3
 cat File3
+
+
+
+
+Name of resultant file is File3. 
+
+
+Hello, This is File1. 
+This file has been created to demonstrate use of filter commnads like HEAD command and TAIL command. 
+The file should atleast contain 6-7 lines as first 5lines have to b.
+I have assigned this work on 26-11-2020 on thursday Lab hours and have to complete it. 
+I have to mke one more file with atleast 16-17 lines because I have to concatenante line 6 to line 15 in that file.
+and contain 80% of the Earth's plant biomass. 
+Net primary production is estimated at 21.9 gigatonnes carbon per year for tropical forests, 8.1 for temperate forests, and 2.6 for boreal forests.
+File does not have 15 lines so Iam adding mmore lines so that file can have more than 15 lines. 
+I have to perform operations from line 6 to line 15 so I need atleast 15 lines to do my work.
+Forests at different latitudes and elevations, 
+and with different precipitation and evapotranspiration form distinctly different biomes: boreal forests around the poles, tropical moist forests and tropical dry forests around the Equator, and temperate forests at the middle latitudes. 
+Higher elevation areas tend to support forests similar to those at higher latitudes, and amount of precipitation also affects forest composition.
