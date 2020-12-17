b6057b8ab4a78597e1a85ca1c6bfecc73e9bec18
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-12-17 15:03:18 +0530

    Add files via upload

---
+++ LAB8/lSeekAPI.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+int main(int argc, char* argv[])
+{
+	char buf;
+	int size, fd;	
+	
+	fd=open(argv[1], O_RDONLY);
+	size=lseek(fd, -1, SEEK_END);
+
+	while(size-- >= 0)
+	{
+		read(fd, &buf, 1);
+		write(STDOUT_FILENO, &buf, 1);
+		lseek(fd, -2, SEEK_CUR);
+	}
+
+return 0;
+}
+
+
+
