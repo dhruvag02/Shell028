7d8d50ecfce56c9fcfc1c73316fe1226a5d208c1
dhruvag02 <53806394+dhruvag02@users.noreply.github.com>
2020-12-24 14:26:03 +0530

    Update execAPI.c

--- LAB9/execAPI.c
+++ LAB9/execAPI.c
@@ -13,7 +13,7 @@ int main()
 	if(pid==0)
 	{
 		printf("In child process\n");
-		execl("/bin/ls", "ls", "-l", (char*)0);
+		execl("/bin/ls", "ls", "-l", "shortlist", (char*)0);
 		printf("\n Attributes of current directories are printed");
 		
 	}
@@ -24,3 +24,9 @@ int main()
 	}
 	return 0;
 }
+
+Before fork()
+In child process
+-rw-r--r-- 1 dhruv dhruv 11 Dec 10 14:29 shortlist
+In Parent process the 'ls' command is not executed
+
